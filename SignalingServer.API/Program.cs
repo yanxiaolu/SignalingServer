57ef39387ad2f074df76b7e93330b8fa274f4c87
yanxiaolu <yanxiaolu@gmail.com>
2024-12-22 17:14:12 +0800

    增加CORS的设置

--- SignalingServer.API/Program.cs
+++ SignalingServer.API/Program.cs
@@ -8,6 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SignalRPolicy", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:5278") // Web Client的地址
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials(); // 允许凭据，SignalR需要这个
+    });
+});
+
 // Add services
 builder.Services.AddSingleton<IRoomService, RoomService>();
 
@@ -18,6 +31,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+// Enable CORS before routing
+app.UseCors("SignalRPolicy");
 
 app.UseHttpsRedirection();
 
